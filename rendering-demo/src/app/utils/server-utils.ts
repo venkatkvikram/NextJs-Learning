9a5d404d6b4240c120b877724dcd4ce9f9918783
Venkata Sai Esampalli <venkatsai.esampalli@gmail.com>
2025-07-18 00:08:58 +0530

    Server-only package

---
+++ rendering-demo/src/app/utils/server-utils.ts
@@ -0,0 +1,9 @@
+export const serverSideFunction = () => {
+    console.log(
+        `use multiple libraries,
+            use environment variables,
+            interact with a database,
+            process confidential information`
+    )
+    return "server result"
+}
