c8398478746724bf7ee9d18aae4b105e251a8488
venkatkvikram <venkatsai.esampalli@gmail.com>
2025-07-18 23:55:37 +0530

    update readme

--- rendering-demo/src/components/nav-links.tsx
+++
@@ -1,4 +0,0 @@
-export default function  NavLinks () {
-    console.log("Nav links rendered")
-    return <h1>List of Navlinks</h1>
-}
--- rendering-demo/src/components/nav-search.tsx
+++
@@ -1,4 +0,0 @@
-export default function NavSearch () {
-    console.log("Nav search rendered")
-    return <h1>Nav Search</h1>
-}
